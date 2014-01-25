4f2627be69082b45f7a371e2fe221862c90802ab
Harlan Harris <harlan@harris.name>
2014-01-25 17:35:39 -0500

    set up / and /admin routes

--- web.js
+++ web.js
@@ -1,5 +1,8 @@
 // web.js
 var express = require("express");
+var http = require('http');
+var path = require('path');
+
 var logfmt = require("logfmt");
 
 var mongo = require('mongodb');
@@ -19,11 +22,25 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.get('/admin', function(req, res) {
+	res.render('admin');
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Listening on " + app.get('port'));
 });
