bdb24bc40bd3bfb81da670b4e10a962f99943f07
Harlan Harris <harlan@harris.name>
2014-01-25 17:17:19 -0500

    add mongo module (mongule?)

--- web.js
+++ web.js
@@ -1,6 +1,20 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+// mongo.Db.connect(mongoUri, function (err, db) {
+//   db.collection('mydocs', function(er, collection) {
+//     collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+//     });
+//   });
+// });
+
 var app = express();
 
 app.use(logfmt.requestLogger());
